50fd59cdc991ec05d9077879cf1268cd8000c1a2
Dan Small <danjsmall@gmail.com>
2026-02-12 16:47:45 -0500

    fixed basepath assignment bug, build should now correctly insert '/boot.dev-SSG/' path.

--- src/main.py
+++ src/main.py
@@ -3,7 +3,8 @@ from generate_page import generate_page, generate_pages_recursive
 import sys
 
 def main():
-    basepath = sys.argv[0] if sys.argv == True else "/"
+
+    basepath = sys.argv[1] if sys.argv[1] else "/"
     check_dirs("./static", "./docs")
     generate_pages_recursive("content", "template.html", "docs", basepath)
 
